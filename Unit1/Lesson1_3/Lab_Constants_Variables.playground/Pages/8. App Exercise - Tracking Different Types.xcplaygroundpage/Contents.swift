16db60581ca9b293ea7a65974bc7f29bd3d1d615
Batoolkhan225 <batoolkhan225@gmail.com>
2025-01-24 19:53:36 -0500

    Update Contents.swift
    working on lab 1.3 pages 1-10

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/8. App Exercise - Tracking Different Types.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/8. App Exercise - Tracking Different Types.xcplaygroundpage/Contents.swift
@@ -6,9 +6,16 @@
 You have declared a number of constants and variables to keep track of fitness information. Declare one more variable with a boolean value called `hasMetStepGoal`.
  */
 
+var hasMetStepGoal = true  // Boolean value to track whether the step goal has been met
 
 //:  When you declared a constant for goal number of steps and a variable for current step count, you likely assigned each a value in the thousands. This can be difficult to read. Redeclare this constant and variable and, when assigning each a value in the thousands, format the number so that it is more readable.
 
+let goalSteps = 10_000  // Formatted constant for goal steps for readability
+var stepsTakenToday = 3_500  // Formatted variable for steps taken today
+
+print("Goal steps: \(goalSteps)")  // Print formatted goal steps
+print("Steps taken today: \(stepsTakenToday)")  // Print formatted steps taken today
+print("Has met step goal: \(hasMetStepGoal)")  // Print whether the goal has been met
 
 /*:
 [Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Inference and Required Values](@next)
