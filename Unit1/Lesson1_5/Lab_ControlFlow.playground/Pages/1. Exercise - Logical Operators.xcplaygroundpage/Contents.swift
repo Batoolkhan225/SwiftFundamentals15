2d64a85ad9709136752c492a31731ac6f6443a19
Batoolkhan225 <batoolkhan225@gmail.com>
2025-01-25 00:13:28 -0500

    Update Contents.swift
    Lab 1.5 page 1 of 9

--- Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
@@ -1,40 +1,48 @@
 /*:
 ## Exercise - Logical Operators
 
- For each of the logical expressions below, print out what you think the resulting value will be (`true` or `false`). Then print out the actual expression to see if you were right. An example has been provided below.
- 
+For each of the logical expressions below, print out what you think the resulting value will be (`true` or `false`). Then print out the actual expression to see if you were right. An example has been provided below.
+
     43 == 53
     print(false)
     print(43 == 53)
+*/
 
- 
- 1. `9 == 9`
- */
+// 1. `9 == 9`
+print(true) // Expected: true
+print(9 == 9) // Actual
 
+// 2. `9 != 9`
+print(false) // Expected: false
+print(9 != 9) // Actual
 
-//:  2. `9 != 9`
+// 3. `47 > 90`
+print(false) // Expected: false
+print(47 > 90) // Actual
 
+// 4. `47 < 90`
+print(true) // Expected: true
+print(47 < 90) // Actual
 
-//:  3. `47 > 90`
+// 5. `4 <= 4`
+print(true) // Expected: true
+print(4 <= 4) // Actual
 
+// 6. `4 >= 5`
+print(false) // Expected: false
+print(4 >= 5) // Actual
 
-//:  4. `47 < 90`
+// 7. `(47 > 90) && (47 < 90)`
+print(false) // Expected: false
+print((47 > 90) && (47 < 90)) // Actual
 
+// 8. `(47 > 90) || (47 < 90)`
+print(true) // Expected: true
+print((47 > 90) || (47 < 90)) // Actual
 
-//:  5. `4 <= 4`
-
-
-//:  6. `4 >= 5`
-
-
-//:  7. `(47 > 90) && (47 < 90)`
-
-
-//:  8. `(47 > 90) || (47 < 90)`
-
-
-//:  9. `!true`
-
+// 9. `!true`
+print(false) // Expected: false
+print(!true) // Actual
 
 /*:
 page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
