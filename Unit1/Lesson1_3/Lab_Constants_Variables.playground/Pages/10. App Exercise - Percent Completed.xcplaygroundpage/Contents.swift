015d57eaa9500fbf74fb208d6ffe278a10cee3ae
Batoolkhan225 <batoolkhan225@gmail.com>
2025-01-24 19:58:44 -0500

    Update Contents.swift
    working on lab 1.3 pages 1-10

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/10. App Exercise - Percent Completed.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/10. App Exercise - Percent Completed.xcplaygroundpage/Contents.swift
@@ -3,12 +3,20 @@
 
  >These exercises reinforce Swift concepts in the context of a fitness tracking app.
 
- You decide that your fitness tracking app should show the user what percentage of his/her goal has been achieved so far today. Declare a variable called `percentCompleted` and set it to 0. Do not explicity assign it a type.
+ You decide that your fitness tracking app should show the user what percentage of his/her goal has been achieved so far today. Declare a variable called `percentCompleted` and set it to 0. Do not explicitly assign it a type.
  */
 
+var percentCompleted = 0  // Implicitly inferred as an Int
 
-//:  Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. This means he/she is 34.67% of the way to his/her goal. Assign 34.67 to `percentCompleted`. Does the code compile? Go back and explicity assign a type to `percentCompleted` that will allow the code to compile.
+//:  Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. This means he/she is 34.67% of the way to his/her goal. Assign 34.67 to `percentCompleted`. Does the code compile? Go back and explicitly assign a type to `percentCompleted` that will allow the code to compile.
 
+percentCompleted = 34.67  // This will not compile because `percentCompleted` is inferred as an Int, and 34.67 is a Double.
+
+// Explicitly assign `percentCompleted` as a Double to make it work.
+
+var percentCompletedExplicitly: Double = 0  // Explicitly declare as a Double
+percentCompletedExplicitly = 34.67  // This will now compile
+print("Percent Completed: \(percentCompletedExplicitly)%")  // Prints "Percent Completed: 34.67%"
 
 /*:
  _Copyright © 2023 Apple Inc._
