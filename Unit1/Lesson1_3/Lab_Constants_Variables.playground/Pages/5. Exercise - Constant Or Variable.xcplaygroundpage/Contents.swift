f86b313396bc07370f26c820ea9565a82b6f4385
Batoolkhan225 <batoolkhan225@gmail.com>
2025-01-24 19:33:13 -0500

    Update Contents.swift
    Workign on lab 1.3 pages 1-10

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -10,7 +10,18 @@
  
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
+// Declaring constants for values that won't change
+let numberOfLikes = 250  // Constant, because the number of likes won't change after it's been recorded
+let numberOfComments = 30  // Constant, because the number of comments is fixed for this post
 
+// Declaring variables for values that might change
+var yearCreated = 2025  // Variable, as the year could change depending on new posts
+var monthCreated = 1  // Variable, the month might be updated if the post is edited
+var dayCreated = 24  // Variable, as the day could be changed (e.g., if the post is reposted)
+
+// Printing the metrics
+print("Post created on: \(monthCreated)/\(dayCreated)/\(yearCreated)")
+print("Likes: \(numberOfLikes), Comments: \(numberOfComments)")
 
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Fitness Tracker: Constant or Variable?](@next)
