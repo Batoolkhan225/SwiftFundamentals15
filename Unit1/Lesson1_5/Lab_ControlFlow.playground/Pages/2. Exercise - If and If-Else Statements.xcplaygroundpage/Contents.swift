db5f3ef711920dc538829c39b2862b3a01da4c82
Batoolkhan225 <batoolkhan225@gmail.com>
2025-01-25 00:16:54 -0500

    Update Contents.swift
    Lab 1.5 page 2 of 9

--- Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/2. Exercise - If and If-Else Statements.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/2. Exercise - If and If-Else Statements.xcplaygroundpage/Contents.swift
@@ -1,18 +1,40 @@
 /*:
 ## Exercise - If and If-Else Statements
- 
- Imagine you're creating a machine that will count your money for you and tell you how wealthy you are based on how much money you have. A variable `dollars` has been given to you with a value of 0. Write an if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0. Observe what is printed to the console.
- */
+
+Imagine you're creating a machine that will count your money for you and tell you how wealthy you are based on how much money you have. A variable `dollars` has been given to you with a value of 0. Write an if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0. Observe what is printed to the console.
+*/
 var dollars = 0
 
+// If statement to check if dollars is 0
+if dollars == 0 {
+    print("Sorry, kid. You're broke!") // Expected: "Sorry, kid. You're broke!"
+}
 
-//:  `dollars` has been updated below to have a value of 10. Write an an if-else statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, but prints "You've got some spending money!" otherwise. Observe what is printed to the console.
+/*:
+`dollars` has been updated below to have a value of 10. Write an if-else statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, but prints "You've got some spending money!" otherwise. Observe what is printed to the console.
+*/
 dollars = 10
 
+// If-else statement to check if dollars is 0 or not
+if dollars == 0 {
+    print("Sorry, kid. You're broke!") // Expected: "Sorry, kid. You're broke!"
+} else {
+    print("You've got some spending money!") // Expected: "You've got some spending money!"
+}
 
-//:  `dollars` has been updated below to have a value of 105. Write an an if-else-if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, prints "You've got some spending money!" if `dollars` is less than 100, and prints "Looks to me like you're rich!" otherwise. Observe what is printed to the console.
+/*:
+`dollars` has been updated below to have a value of 105. Write an if-else-if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, prints "You've got some spending money!" if `dollars` is less than 100, and prints "Looks to me like you're rich!" otherwise. Observe what is printed to the console.
+*/
 dollars = 105
 
+// If-else-if statement to check different ranges of dollar amounts
+if dollars == 0 {
+    print("Sorry, kid. You're broke!") // Expected: "Sorry, kid. You're broke!"
+} else if dollars < 100 {
+    print("You've got some spending money!") // Expected: "You've got some spending money!"
+} else {
+    print("Looks to me like you're rich!") // Expected: "Looks to me like you're rich!"
+}
 
 /*:
 [Previous](@previous)  |  page 2 of 9  |  [Next: App Exercise - Fitness Decisions](@next)
