22fa1212dcac50e8354eb588887b106825fd12ef
Batoolkhan225 <batoolkhan225@gmail.com>
2025-02-07 16:32:24 -0500

    created light project folder

---
+++ Unit1/GuidedProject_Light/Light/Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by user269443 on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
